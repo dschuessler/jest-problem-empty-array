78a4b35a4cfda0959719f499730fb968eec58c7b
Dennis Schüßler <github.ds@web.de>
2021-01-05 12:39:36 +0100

    Makes example more concise

--- __mocks__/dbConfig.ts
+++ __mocks__/dbConfig.ts
@@ -1,13 +1,3 @@
 export const dbConfig = {
-  type: "postgres",
-  host: "db",
-  port: 5432,
-  username: "user",
-  password: "pw123",
-  database: "database",
   entities: ["entities/path/of/mock"],
-  keepConnectionAlive: true,
-  synchronize: false,
-  // Log queries which take longer than 1s
-  maxQueryExecutionTime: 1000,
 };
